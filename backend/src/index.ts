030713345c970a0ffe3aec497525e9f0a3ca7d99
CarlosPerez1999 <carlos.al.perez.hdez99@gmail.com>
2025-05-30 20:47:48 -0600

    feat(backend): Add initial doctor routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import PatientRoutes from "./routes/patient.routes";
+import DoctorRoutes from "./routes/doctor.routes";
 
 const app = express()
 
@@ -8,6 +9,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/patients", PatientRoutes)
+app.use("/doctors", DoctorRoutes)
 
 app.listen(3000, () => {
   console.log(`Server running on http://localhost:3000`)
---
+++ backend/src/routes/doctor.routes.ts
@@ -0,0 +1,28 @@
+import express, {Request, Response} from "express";
+import {DoctorController} from "../controllers/doctor.controller";
+
+const router = express.Router();
+const doctorController = new DoctorController()
+
+router.get("/", async (req: Request, res: Response) => {
+      await doctorController.getDoctors(req, res)
+    }
+)
+router.get("/:id", async (req: Request, res: Response) => {
+      await doctorController.getDoctorById(req, res)
+    }
+)
+router.post("/", async (req: Request, res: Response) => {
+      await doctorController.addDoctor(req, res)
+    }
+)
+router.delete("/:id", async (req: Request, res: Response) => {
+      await doctorController.deleteDoctor(req, res)
+    }
+)
+router.patch("/:id", async (req: Request, res: Response) => {
+      await doctorController.updateDoctor(req, res)
+    }
+)
+
+export default router;
